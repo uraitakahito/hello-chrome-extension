e09fcd8fdebdd7c7caac51cd777f72274e079cfe
Takahito Urai <uraitakahito@gmail.com>
2024-12-16 23:13:11 +0900

    Add Vite configuration file for project build setup

---
+++ vite.config.mjs
@@ -0,0 +1,22 @@
+// Vitest supports the same extensions for your configuration file as Vite does:
+// .js, .mjs, .cjs, .ts, .cts, .mts.
+
+import { resolve } from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        // https://rollupjs.org/configuration-options/#output-entryfilenames
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
+});
